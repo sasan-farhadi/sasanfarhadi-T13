655e06216d095c432eece36efc4d81df0af00b44
Sasan <sasanfarhadi@gmail.com>
2024-08-22 00:54:14 +0330

    The header section is complete

---
+++ src/Tab1.jsx
@@ -0,0 +1,21 @@
+const Tab1 = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="content">
+                    <h2>JavaScript History : </h2>
+                    <div className="article">
+                        <p>
+                            Creation at Netscape
+                            The first popular web browser with a graphical user interface, Mosaic, was released in 1993. Accessible to non-technical people, it played a prominent role in the rapid growth of the early World Wide Web.<br></br> The lead developers of Mosaic then founded the Netscape corporation, which released a more polished browser, Netscape Navigator, in 1994. This quickly became the most-used.<br></br>
+                            During these formative years of the Web, web pages could only be static, lacking the capability for dynamic behavior after the page was loaded in the browser.<br></br> There was a desire in the flourishing web development scene to remove this limitation, so in 1995, Netscape decided to add a programming language to Navigator. They pursued two routes to achieve this: collaborating with Sun Microsystems to embed the Java language, while also hiring Brendan Eich to embed the Scheme language.
+                        </p>
+                        <img src="/js.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Tab1
---
+++ src/Tab2.jsx
@@ -0,0 +1,19 @@
+const Tab1 = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="content">
+                    <h2>React (JavaScript library) : </h2>
+                    <div className="article">
+                        <p>React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on components by Facebook Inc.<br></br> It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.<br></br>
+
+React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.<br></br> A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary rerendering of unchanged DOM elements. It was first launched on 29 May 2013.</p>
+                        <img src="/reactjs.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Tab1
